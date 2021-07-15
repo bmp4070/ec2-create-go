39e1cd313fda90e11d01bec0656ce4ff63239dce
Medha Bhargav Prabhala <mprabhala@salesforce.com>
2021-07-15 00:26:58 -0700

    Create EC2 VMs from Lambda using go handler

---
+++ sample-apps/ec2-create-go/function/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	runtime "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+
+	"fmt"
+	"log"
+)
+
+func handleRequest(ctx context.Context, event events.SQSEvent) (string, error) {
+	// event
+	eventJson, _ := json.MarshalIndent(event, "", "  ")
+	log.Printf("EVENT: %s", eventJson)
+	// environment variables
+	log.Printf("REGION: %s", os.Getenv("AWS_REGION"))
+	log.Println("ALL ENV VARS:")
+	for _, element := range os.Environ() {
+		log.Println(element)
+	}
+	// request context
+	lc, _ := lambdacontext.FromContext(ctx)
+	log.Printf("REQUEST ID: %s", lc.AwsRequestID)
+	// global variable
+	log.Printf("FUNCTION NAME: %s", lambdacontext.FunctionName)
+	// context method
+	deadline, _ := ctx.Deadline()
+	log.Printf("DEADLINE: %s", deadline)
+
+	session, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2")},
+	)
+	if err != nil {
+		fmt.Println("Could not create session", err)
+	}
+	// Create EC2 service client
+	svc := ec2.New(session)
+
+	// Specify the details of the instance that you want to create.
+	runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
+		// An Amazon Linux AMI ID for t2.micro instances in the us-west-2 region
+		ImageId:      aws.String("ami-e7527ed7"),
+		InstanceType: aws.String("t2.micro"),
+		MinCount:     aws.Int64(1),
+		MaxCount:     aws.Int64(1),
+	})
+
+	if err != nil {
+		fmt.Println("Could not create instance", err)
+		return "Could not create instance", err
+	}
+
+	fmt.Println("Created instance", *runResult.Instances[0].InstanceId)
+
+	// Add tags to the created instance
+	_, errtag := svc.CreateTags(&ec2.CreateTagsInput{
+		Resources: []*string{runResult.Instances[0].InstanceId},
+		Tags: []*ec2.Tag{
+			{
+				Key:   aws.String("Name"),
+				Value: aws.String("MyFirstInstance"),
+			},
+		},
+	})
+	if errtag != nil {
+		log.Println("Could not create tags for instance", runResult.Instances[0].InstanceId, errtag)
+		return "Could not create tags for instance", errtag
+	}
+
+	fmt.Println("Successfully tagged instance")
+	return *runResult.Instances[0].InstanceId, nil
+}
+
+func main() {
+	runtime.Start(handleRequest)
+}
